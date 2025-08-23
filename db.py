daeb139d1372ece443707683d203285ca8714b40
VIPERJOHN6 <132152142+VIPERJOHN6@users.noreply.github.com>
2025-08-23 21:53:55 +0100

    second commit

--- db.py
+++ db.py
@@ -1,1 +1,11 @@
 print("Database setup goes here")
+
+import pymysql as pyms
+
+
+my_con = pyms.connect(host= 'localhost', user ='root', database = 'todolist' , passwd='Cassanova911@')
+print(my_con)
+print('connection succefully')
+
+myCursor = my_con.cursor()
+print("done")
